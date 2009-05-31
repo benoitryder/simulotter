8364600db5662c4fdcdb962fc0c1149435ea03d4
Benoît Ryder <benoit@ryder.fr>
2009-05-31 12:06:01 +0000

    Use Unix-like EOL. Rewrite/cleaned SmartPtr class.

--- smart.h
+++ smart.h
@@ -5,96 +5,53 @@
 
 /** @brief Smart pointer class
  *
- * Implementation is the same as boost::intrusive_ptr.
- * Some features have been removed.
+ * Implementation is very similar to boost::intrusive_ptr.
  * Implicit cast to raw pointers has been added.
  * add_ref and release methods have been added.
  */
 template<class T>
 class SmartPtr
 {
-private:
-  typedef SmartPtr this_type;
-
 public:
+  SmartPtr(): px(0) {}
+  SmartPtr(T *p): px(p) { if(px!=0) SmartPtr_add_ref(px); }
+  SmartPtr(SmartPtr const &rhs): px(rhs.px) { if(px!=0) SmartPtr_add_ref(px); }
+  ~SmartPtr() { if(px!=0) SmartPtr_release(px); }
 
-  SmartPtr(): px( 0 )
+  SmartPtr &operator=(SmartPtr const &rhs)
   {
-  }
-
-  SmartPtr( T * p, bool do_add_ref = true ): px( p )
-  {
-    if( px != 0 && do_add_ref ) SmartPtr_add_ref( px );
-  }
-
-#if !defined(BOOST_NO_MEMBER_TEMPLATES) || defined(BOOST_MSVC6_MEMBER_TEMPLATES)
-  template<class U> SmartPtr( SmartPtr<U> const & rhs ) : px( rhs.get() )
-  {
-    if( px != 0 ) SmartPtr_add_ref( px );
-  }
-#endif
-
-  SmartPtr(SmartPtr const & rhs): px( rhs.px )
-  {
-    if( px != 0 ) SmartPtr_add_ref( px );
-  }
-
-  ~SmartPtr()
-  {
-    if( px != 0 ) SmartPtr_release( px );
-  }
-
-#if !defined(BOOST_NO_MEMBER_TEMPLATES) || defined(BOOST_MSVC6_MEMBER_TEMPLATES)
-  template<class U> SmartPtr & operator=(SmartPtr<U> const & rhs)
-  {
-    this_type(rhs).swap(*this);
+    SmartPtr(rhs).swap(*this);
     return *this;
   }
-#endif
-
-  SmartPtr & operator=(SmartPtr const & rhs)
+  SmartPtr &operator=(T *rhs)
   {
-    this_type(rhs).swap(*this);
+    SmartPtr(rhs).swap(*this);
     return *this;
   }
 
-  SmartPtr & operator=(T * rhs)
-  {
-    this_type(rhs).swap(*this);
-    return *this;
-  }
-
-  void reset() { this_type().swap( *this ); }
-  void reset( T * rhs ) { this_type( rhs ).swap( *this ); }
-
-  T * get() const { return px; }
-  T & operator*() const { return *px; }
-  T * operator->() const { return px; }
+  T *get() const { return px; }
+  T &operator*() const { return *px; }
+  T *operator->() const { return px; }
   operator T *() const { return px; }
 
-  void swap(SmartPtr & rhs)
+  void swap(SmartPtr &rhs)
   {
-    T * tmp = px;
+    T *tmp = px;
     px = rhs.px;
     rhs.px = tmp;
   }
 
 private:
-
-  T * px;
+  T *px;
 };
 
-template<class T, class U> inline bool operator==(SmartPtr<T> const & a, SmartPtr<U> const & b) { return a.get() == b.get(); }
-template<class T, class U> inline bool operator!=(SmartPtr<T> const & a, SmartPtr<U> const & b) { return a.get() != b.get(); }
-template<class T, class U> inline bool operator==(SmartPtr<T> const & a, U * b) { return a.get() == b; }
-template<class T, class U> inline bool operator!=(SmartPtr<T> const & a, U * b) { return a.get() != b; }
-template<class T, class U> inline bool operator==(T * a, SmartPtr<U> const & b) { return a == b.get(); }
-template<class T, class U> inline bool operator!=(T * a, SmartPtr<U> const & b) { return a != b.get(); }
-#if __GNUC__ == 2 && __GNUC_MINOR__ <= 96
-// Resolve the ambiguity between our op!= and the one in rel_ops
-template<class T> inline bool operator!=(SmartPtr<T> const & a, SmartPtr<T> const & b) { return a.get() != b.get(); }
-#endif
-template<class T> inline bool operator<(SmartPtr<T> const & a, SmartPtr<T> const & b) { return a.get() < b.get(); }
+template<class T, class U> inline bool operator==(SmartPtr<T> const &a, SmartPtr<U> const &b) { return a.get() == b.get(); }
+template<class T, class U> inline bool operator!=(SmartPtr<T> const &a, SmartPtr<U> const &b) { return a.get() != b.get(); }
+template<class T, class U> inline bool operator==(SmartPtr<T> const &a, U *b) { return a.get() == b; }
+template<class T, class U> inline bool operator!=(SmartPtr<T> const &a, U *b) { return a.get() != b; }
+template<class T, class U> inline bool operator==(T *a, SmartPtr<U> const &b) { return a == b.get(); }
+template<class T, class U> inline bool operator!=(T *a, SmartPtr<U> const &b) { return a != b.get(); }
+template<class T> inline bool operator<(SmartPtr<T> const &a, SmartPtr<T> const &b) { return a.get() < b.get(); }
 
 
 /** @brief Base class for objects with ref count.
